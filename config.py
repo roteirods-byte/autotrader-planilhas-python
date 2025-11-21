dbcf6426c448b5511e2801a9de081efb0cdd516f
JORGE SOBRINHO <roteiro.ds@gmail.com>
2025-11-21 10:45:21 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 import os
 from zoneinfo import ZoneInfo
 
-# Fuso horário padrão do projeto (Brasil / São Paulo)
+# Fuso horário oficial do projeto (Brasil/São Paulo)
 TZINFO = ZoneInfo("America/Sao_Paulo")
 
-# Casas decimais padrão usadas no worker_entrada
-PRICE_DECIMALS = int(os.getenv("PRICE_DECIMALS", 3))
-PCT_DECIMALS = int(os.getenv("PCT_DECIMALS", 2))
+# Casas decimais usadas no worker_entrada
+PRICE_DECIMALS = int(os.getenv("PRICE_DECIMALS", "3"))
+PCT_DECIMALS = int(os.getenv("PCT_DECIMALS", "2"))
